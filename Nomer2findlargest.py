9671c83a93ae8ef4d7732a40e270b17c9e49a459
harlanfadhilah69 <harlancoegg@gmail.com>
2024-10-14 16:48:12 +0700

    2

--- Nomer2findlargest.py
+++ Nomer2findlargest.py
@@ -6,7 +6,7 @@ def find_largest(a, b, c):
     else:
         print (c)
 
-# Example usage:
+
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 c = float(input("Enter third number: "))
