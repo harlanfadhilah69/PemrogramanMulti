f9dab965d0385a1b3ee02d7bdb1f779911d973f8
harlanfadhilah69 <harlancoegg@gmail.com>
2024-10-14 16:45:44 +0700

    nomer5

--- Nomer5.py
+++ Nomer5.py
@@ -1,7 +1,7 @@
 def print_design(n):
-    for i in range(1, n+1):  # Loop through 1 to n
-        print(f"{i} " * i)  # Print 'i' repeated 'i' times followed by a space
+    for i in range(1, n+1):  
+        print(f"{i} " * i)  
 
-# Example usage:
+
 n = int(input("Enter the value of n: "))
 print_design(n)
